a8827bc40b8a17362f96ccf5668aa657198de285
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-03-22 05:58:20 +0530

    Complete news article listing styling

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -18,11 +18,27 @@ class NewsSources extends Component {
         {this.props.news.news &&
           this.props.news.news.map(news => (
             <div className="card">
-              <div className="card-header">Associated Press</div>
+              <div className="card-header">
+                <span className="news-article-source">{news.source.name}</span>
+                <span>
+                  {news.publishedAt.replace("T", " ").replace("Z", "")}
+                </span>
+              </div>
               <div className="card-body">
                 <div>
-                  <h5 className="card-title">{news.title}</h5>
+                  <h5 className="card-title">
+                    <a
+                      href={news.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {news.title}
+                    </a>
+                  </h5>
                   <p className="card-text">{news.description}</p>
+                  {news.author && (
+                    <p className="card-text">{news.author.toUpperCase()}</p>
+                  )}
                   <a
                     href={news.url}
                     target="_blank"
